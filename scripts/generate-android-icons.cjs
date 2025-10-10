98f8fe0acef9cd6b0dc8e85abebf4eb2cd53b035
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-10-10 13:41:37 +0530

    Update generate-android-icons.cjs

--- scripts/generate-android-icons.cjs
+++ scripts/generate-android-icons.cjs
@@ -18,7 +18,8 @@ const sizes = {
 (async () => {
   for (const [folder, size] of Object.entries(sizes)) {
     const dir = path.join(RES_PATH, folder);
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    if (fs.existsSync(dir)) fs.rmSync(dir, { recursive: true, force: true });
+    fs.mkdirSync(dir, { recursive: true });
 
     await sharp(ICON_PATH)
       .resize(size, size)
