c76cc3d9c9d21354bb28a226d90e9517262a998e
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-10-17 12:16:55 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,15 +2,20 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.celestial.oracle',
-  appName: 'Cosmic Astrology Ai',
+  appName: 'Cosmic Astrology AI',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
   },
+  android: {
+    buildOptions: {
+      packageType: 'bundle'
+    }
+  },
   plugins: {
     AdMob: {
-      appId: 'ca-app-pub-3375938019790298~8473700663',
-      testingDevices: [],
+      appId: 'ca-app-pub-3375938019790298~8473700666',
+      testingDevices: []
     }
   }
 };
